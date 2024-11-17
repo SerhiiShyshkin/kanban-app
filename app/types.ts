f32a4567cb5073887482965b8ca3ce2c9666eeba
Serhii Shyshkin <s.b.shyshkin@gmail.com>
2024-11-17 17:56:01 +0100

    Added types for project from prisma models

--- app/types.ts
+++ app/types.ts
@@ -1,9 +1,20 @@
 import { Prisma } from "@prisma/client";
 
 type Board = Prisma.BoardGetPayload<{
-  select: { id: true; title: true; columns: { select: { title: true; color: true } } };
+  select: { id: true; title: true; columns: { select: { id: true; title: true; color: true } } };
 }>;
 
-type Column = Prisma.ColumnGetPayload<{ select: { title: true; color: true } }>;
+type Column = Prisma.ColumnGetPayload<{ select: { id: true; title: true; color: true } }>;
 
-export type {Board, Column };
+type Task = Prisma.TaskGetPayload<{
+  select: {
+    id: true;
+    title: true;
+    description: true;
+    subtasks: { select: { id: true; title: true; isCompleted: true } };
+  };
+}>;
+
+type Subtask = Prisma.SubtaskGetPayload<{ select: { id: true; title: true; isCompleted: true } }>;
+
+export type { Board, Column, Task, Subtask };
