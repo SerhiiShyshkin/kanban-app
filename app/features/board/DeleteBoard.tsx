961e5c53910e47a5cc0b2e1673fb0752a3d1a1ef
Serhii Shyshkin <s.b.shyshkin@gmail.com>
2024-12-01 11:00:17 +0100

    Modified prettier.xml

--- app/features/board/DeleteBoard.tsx
+++ app/features/board/DeleteBoard.tsx
@@ -1,38 +1,36 @@
 'use client'
 
+
 import DeleteForm from "@/app/components/DeleteForm";
 import Modal from "@/app/components/Modal";
-
 import useToggleOpen from "@/lib/hooks/useToggleOpen";
-import { deleteBoardById } from "@/lib/server-actions/board-actions";
+import {deleteBoardById} from "@/lib/server-actions/board-actions";
 
 
 type DeleteBoardProps = {
-  id: string;
+    id: string;
 
 };
 
-const DeleteBoard = ({ id }: DeleteBoardProps) => {
-  const { isOpen, setIsOpen } = useToggleOpen();
+const DeleteBoard = ({id}: DeleteBoardProps) => {
+    const {isOpen, setIsOpen} = useToggleOpen();
 
 
-
-
-  return (
-    <>
-      <button  onClick={() => setIsOpen(true)}>Delete</button>
-      {isOpen && (
-        <Modal setIsOpen={setIsOpen}>
-          <DeleteForm
-            title="Delete this Board?"
-            message="Are you sure you want to delete the ‘Platform Launch’ board? This action will remove all columns and tasks and cannot be reversed."
-            onCancel={setIsOpen}
-            onSubmit={() => deleteBoardById(id)}
-          />
-        </Modal>
-      )}
-    </>
-  );
+    return (
+        <>
+            <button onClick={() => setIsOpen(true)}>Delete</button>
+            {isOpen && (
+                <Modal setIsOpen={setIsOpen}>
+                    <DeleteForm
+                        title="Delete this Board?"
+                        message="Are you sure you want to delete the ‘Platform Launch’ board? This action will remove all columns and tasks and cannot be reversed."
+                        onCancel={setIsOpen}
+                        onSubmit={() => deleteBoardById(id)}
+                    />
+                </Modal>
+            )}
+        </>
+    );
 };
 
 export default DeleteBoard;
